b7611d2ded245354d24ce1b97a0bdeac46917a25
Jayesh7744 <jayeshs7744@gmail.com>
2022-05-21 23:06:46 +0530

    Added: updated

--- sumAndAverage.sh
+++ sumAndAverage.sh
@@ -1,9 +1,21 @@
-echo "The 1st random number is:" $(( 25 + RANDOM % 76 ))
 
-echo "The 2nd random number is:" $(( 25 + RANDOM % 76 ))
+a=$(( 25 + RANDOM % 76 ))
+echo "The 1st random number is:" $a
 
-echo "The 3rd random number is:" $(( 25 + RANDOM % 76 ))
+b=$(( 25 + RANDOM % 76 ))
+echo "The 2nd random number is:" $b
 
-echo "The 4th random number is:" $(( 25 + RANDOM % 76 ))
+c=$(( 25 + RANDOM % 76 ))
+echo "The 3rd random number is:" $c
 
-echo "The 5th random number is:" $(( 25 + RANDOM % 76 ))
+d=$(( 25 + RANDOM % 76 ))
+echo "The 4th random number is:" $d
+
+e=$(( 25 + RANDOM % 76 ))
+echo "The 5th Random Number is:" $e
+
+sum=$(( $a + $b + $c + $d + $e ))
+echo "The sum of all five numbers is:" $sum
+
+average=$(( $sum % 5 ))
+echo "Average of all five numbers is:" $average
